bd724d11055679ac5c906949e5dc26d6fe21b59e
Eithar-Yahia <eitharyahia444@gmail.com>
2021-03-23 06:03:50 +0200

    adding auth

--- routes/web.php
+++ routes/web.php
@@ -14,18 +14,24 @@ use App\Http\Controllers\PostController;
 |
 */
 
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
 //my default page with / will return index view with all posts
-Route::get('/', [PostController::class, 'index'])->name('posts.index');
+Route::get('/', [PostController::class, 'index'])->name('posts.index')->middleware('auth');
 //to view crete post page
-Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create')->middleware('auth');
 //to save data from create post page
-Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+Route::post('/posts', [PostController::class, 'store'])->name('posts.store')->middleware('auth');
 //to view edit post using postId as a dynamic parameter
-Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
+Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit')->middleware('auth');
 //to save editing
-Route::put('/posts/{post}', [PostController::class, 'update'])->name('posts.update');
+Route::put('/posts/{post}', [PostController::class, 'update'])->name('posts.update')->middleware('auth');
 //to view single post using postId as a dynamic parameter
-Route::get('/{post}', [PostController::class, 'show'])->name('posts.show');
+Route::get('/{post}', [PostController::class, 'show'])->name('posts.show')->middleware('auth');
 //delete
 Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
 
